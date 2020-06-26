8b8ab5a5f3a9b6f674be33d9e7326a22d948f654
chris <test3teamapp@gmail.com>
2020-06-27 00:10:21 +0300

    deployed to firebase as gospyapp.web.app  TODO: HTTPS calls to backend go services

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  wsEndpoint: 'ws://localhost:8081/api/v1/ws',
+  wsEndpoint: 'ws://158.101.171.124:8081/api/v1/ws',
   reconnectInterval: 2000
 };
