c313f5b34513310f0f8d98ce05f76dabc50beb97
chris <test3teamapp@gmail.com>
2020-06-26 21:39:20 +0300

    list of laste records is brought and displayed. click on row puts marker. button for clrearing markers.

--- src/app/maps/maps.component.ts
+++ src/app/maps/maps.component.ts
@@ -1,12 +1,19 @@
 import { Component, SimpleChanges, OnInit, OnDestroy } from "@angular/core";
 import { Subscription } from "rxjs/Subscription";
 import { map, catchError, tap, switchAll } from "rxjs/operators";
-import { Spyrecord, HttpService } from "../websocket/httpservice.service";
+import {
+  Spyrecord,
+  SpyrecordClass,
+  HttpService,
+} from "../websocket/httpservice.service";
 import { NguiMapModule, NguiMapComponent } from "@ngui/map";
+import { HttpClient } from "@angular/common/http";
+import { HttpErrorResponse, HttpResponse } from "@angular/common/http";
+import { isUndefined } from "util";
 
 declare interface TableData {
   headerRow: string[];
-  dataRows: string[][];
+  dataRows: SpyrecordClass[];
 }
 
 @Component({
@@ -26,7 +33,7 @@ export class MapsComponent implements OnInit, OnDestroy {
   googleMapObject: any;
 
   public tableData1: TableData;
-  public tableData2: TableData;
+
   // allOptions = {
   //   center: {lat: 36.964, lng: -122.015},
   //   zoom: 18,
@@ -34,47 +41,164 @@ export class MapsComponent implements OnInit, OnDestroy {
   //   tilt: 45
   // };
 
-  constructor(private httpService: HttpService) {}
+  constructor(private httpService: HttpService, private http: HttpClient) {}
 
   clear() {
-    this.httpService = undefined;
+    //this.httpService = undefined;
     this.error = undefined;
     this.headers = undefined;
+    this.spyrecord = undefined;
   }
 
-  showLastSpyrecordForUser() {
-    this.httpService.getLastSpyrecordOfUser("UNKNOWN").subscribe(
-      (data: Spyrecord) =>
-        (this.spyrecord = {
-          serialkey: data["Serialkey"],
-          servertime: data["Servertime"],
-          userid: data["Userid"],
-          jsondata: data["Jsondata"],
-          lat: data["Lat"],
-          lng: data["Lng"],
-        }), // success path
-      (error) => (this.error = error) // error path
+  clearMarkers() {
+    //this.httpService = undefined;
+    this.positions = [];
+  }
+
+  showLastSpyrecordForUser(userselected: string) {
+    this.clear();
+
+    this.httpService.getLastSpyrecordOfUser(userselected).subscribe(
+      (data: any) => {
+        // success path
+
+        if (data.message) {
+          // not the result we expected
+          console.log(" Response message: " + data.message);
+          this.error = data.message;
+          //reject(res);
+        } else {
+          this.spyrecord = {
+            serialkey: data["Serialkey"],
+            servertime: data["Servertime"],
+            userid: data["Userid"],
+            jsondata: data["Jsondata"],
+            lat: data["Lat"],
+            lng: data["Lng"],
+          };
+        }
+
+        if (this.spyrecord !== undefined) {
+          //check that parsing is done ok
+          this.positions.push([this.spyrecord.lat, this.spyrecord.lng]);
+
+          this.googleMapObject.panTo(
+            new google.maps.LatLng({
+              lat: this.spyrecord.lat,
+              lng: this.spyrecord.lng,
+            })
+          );
+          this.googleMapObject.zoom = 18;
+        }
+      },
+      (error) => (this.error = error), // error path
+      () => {
+        console.log("http call finished");
+        console.log("spyrecord: " + this.spyrecord);
+        console.log("positions: " + this.positions);
+        console.log("error: " + this.error);
+      }
     );
+  }
 
-    console.log(this.spyrecord);
-    console.log(this.positions);
+  getListOfLastSpyrecordsForAllUsers(usersselected: string) {
+    this.clear();
 
-    if (this.spyrecord) {
-      this.positions.push([this.spyrecord.lat, this.spyrecord.lng]);
+    this.httpService.getLastSpyrecordsOfUsers(usersselected).subscribe(
+      (data: any) => {
+        // success path
 
-      this.googleMapObject.panTo(
-        new google.maps.LatLng({
-          lat: this.spyrecord.lat,
-          lng: this.spyrecord.lng,
+        if (data.message) {
+          // not the result we expected
+          console.log(" Response message: " + data.message);
+          this.error = data.message;
+          //reject(res);
+        } else {
+
+          this.tableData1.dataRows = data.map((item) => {
+            //console.log(item["Userid"]);
+            return new SpyrecordClass(
+              item["Serialkey"],
+              item["Servertime"],
+              item["Userid"],
+              item["Jsondata"],
+              item["Lat"],
+              item["Lng"],
+            );
+          });
+
+        }
+
+      },
+      (error) => (this.error = error), // error path
+      () => {
+        console.log("http call finished");
+        console.log("table rows number: " + this.tableData1.dataRows.length);
+        console.log("error: " + this.error);
+      }
+    );
+  }
+
+  showLastSpyrecordForUserPromise(userselected: string) {
+    // initialise
+    this.clear();
+
+    let promise = new Promise((resolve, reject) => {
+      //TODO
+      this.http
+        .get(this.httpService.endpointUrl + "user/" + userselected, {
+          responseType: "json",
         })
-      );
-      this.googleMapObject.zoom = 18;
-      //this.map.center = new google.maps.LatLng({lat: this.spyrecord.lat, lng: this.spyrecord.lng});
-      //this.map.setCenter();
-      //this.map.center_changed = true;
-      //this.allOptions.center = {lat: this.spyrecord.lat, lng: this.spyrecord.lng};
-      //this.allOptions.zoom = 15;
-    }
+        .toPromise()
+        .then(
+          (res: any) => {
+            // Success
+            console.log("Promise response: " + res);
+            //var checkRes = JSON.parse(res);
+            if (res.message) {
+              // not the result we expected
+              console.log("Promise response message: " + res.message);
+              this.error = res.message;
+              //reject(res);
+            } else {
+              this.spyrecord = {
+                serialkey: res["Serialkey"],
+                servertime: res["Servertime"],
+                userid: res["Userid"],
+                jsondata: res["Jsondata"],
+                lat: res["Lat"],
+                lng: res["Lng"],
+              };
+
+              if (this.spyrecord !== undefined) {
+                this.positions.push([this.spyrecord.lat, this.spyrecord.lng]);
+
+                this.googleMapObject.panTo(
+                  new google.maps.LatLng({
+                    lat: this.spyrecord.lat,
+                    lng: this.spyrecord.lng,
+                  })
+                );
+                this.googleMapObject.zoom = 18;
+              }
+            }
+
+            console.log("spyrecord: " + String(this.spyrecord));
+            console.log("positions: " + this.positions);
+            console.log("error: " + this.error);
+
+            resolve();
+          },
+          (err: HttpErrorResponse) => {
+            // Error
+            console.log("Promise error: " + err.message);
+            this.error = err;
+            reject(err);
+          }
+        );
+    });
+
+    return promise;
   }
 
   onMapReady(map) {
@@ -100,11 +224,10 @@ export class MapsComponent implements OnInit, OnDestroy {
   ngOnInit() {
     this.tableData1 = {
       headerRow: ["ID", "last seen", "lat", "lng"],
-      dataRows: [
-        
-      ],
+      dataRows: [],
     };
-    
+
+    this.getListOfLastSpyrecordsForAllUsers("all");
   }
 
   ngOnDestroy() {}
--- src/app/websocket/httpservice.service.ts
+++ src/app/websocket/httpservice.service.ts
@@ -4,6 +4,8 @@ import { HttpErrorResponse, HttpResponse } from "@angular/common/http";
 
 import { Observable, throwError } from "rxjs";
 import { catchError, retry } from "rxjs/operators";
+import "rxjs/add/operator/toPromise";
+import { getJSON } from "@ngui/map/services/util";
 
 export interface Spyrecord {
   serialkey: number;
@@ -13,12 +15,28 @@ export interface Spyrecord {
   lat: number;
   lng: number;
 }
+export class SpyrecordClass {
+  constructor(
+    public serialkey: number,
+    public servertime: string,
+    public userid: string,
+    public jsondata: string,
+    public lat: number,
+    public lng: number
+  ) {}
+}
 
 @Injectable()
 export class HttpService {
-  endpointUrl = "http://127.0.0.1:8081/api/v1/";
+  //endpointUrl = "http://127.0.0.1:8081/api/v1/";
+  endpointUrl = "http://158.101.171.124:8081/api/v1/";
+  results: SpyrecordClass[];
+  loading: boolean;
 
-  constructor(private http: HttpClient) {}
+  constructor(private http: HttpClient) {
+    this.results = [];
+    this.loading = false;
+  }
 
   getLastSpyrecordOfUser(userId: string) {
     return this.http
@@ -26,9 +44,63 @@ export class HttpService {
         responseType: "json",
       })
       .pipe(
-        retry(3), // retry a failed request up to 3 times
+        //retry(3), // retry a failed request up to 3 times
         catchError(this.handleError) // then handle the error
       );
+  }
+
+
+  getLastSpyrecordsOfUsers(userIds: string) {
+    return this.http
+      .get(this.endpointUrl + "users/" + userIds, {
+        responseType: "json",
+      })
+      .pipe(
+        //retry(3), // retry a failed request up to 3 times
+        catchError(this.handleError) // then handle the error
+      );
+  }
+
+
+  getPromiseLastSpyrecordOfUser(userId: string) {
+    let promise = new Promise((resolve, reject) => {
+      //TODO
+      this.http
+        .get(this.endpointUrl + "user/" + userId, {
+          responseType: "json",
+        })
+        .toPromise()
+        .then(
+          (res: any) => {
+            // Success
+            console.log(res);
+            //var checkRes = JSON.parse(res);
+            if (res.message){
+              // not the result we expected
+              console.log(res.message);
+              reject(res.message);
+            }
+
+            // this.results = res.json().map((item: any) => {
+            //   return new SpyrecordClass(
+            //     item.serialkey,
+            //     item.servertime,
+            //     item.userid,
+            //     item.jsondata,
+            //     item.lat,
+            //     item.lng
+            //   );
+            // });
+            resolve(res);
+          },
+          (err) => {
+            // Error
+            reject(err);
+          }
+        );
+    });
+
+    return promise;
   }
 
   private handleError(error: HttpErrorResponse) {
