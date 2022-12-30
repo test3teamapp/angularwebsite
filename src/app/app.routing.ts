b3eafa011464b53d5f8f20da07f42ada7afc692e
test3teamapp <test3teamapp@gmail.com>
2022-12-30 20:48:53 +0000

    updated local angular modules to 15

--- src/app/app.routing.ts
+++ src/app/app.routing.ts
@@ -21,7 +21,9 @@ const routes: Routes =[
     children: [
         {
       path: '',
-      loadChildren: './layouts/admin-layout/admin-layout.module#AdminLayoutModule'
+      //loadChildren: './layouts/admin-layout/admin-layout.module#AdminLayoutModule'
+      // for angular 15 
+      loadChildren:() => import('./layouts/admin-layout/admin-layout.module').then(x => x.AdminLayoutModule)
   }]},
   { 
     path: 'account',
