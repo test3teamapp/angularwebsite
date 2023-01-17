bcdbacdfafe6a10dd24927a7ba15bf583ea4e555
test3teamapp <test3teamapp@gmail.com>
2023-01-18 00:40:38 +0200

    fixed "logout" on mobile site

--- src/app/sidebar/sidebar.component.ts
+++ src/app/sidebar/sidebar.component.ts
@@ -1,21 +1,22 @@
 import { Component, OnInit } from '@angular/core';
+import { AccountService } from '../_services/account.services';
 
 declare const $: any;
 declare interface RouteInfo {
-    path: string;
-    title: string;
-    icon: string;
-    class: string;
+  path: string;
+  title: string;
+  icon: string;
+  class: string;
 }
 export const ROUTES: RouteInfo[] = [
-    { path: '/dashboard', title: 'Dashboard',  icon: 'pe-7s-graph', class: '' },
-    //{ path: '/user', title: 'User Profile',  icon:'pe-7s-user', class: '' },
-    //{ path: '/table', title: 'Table List',  icon:'pe-7s-note2', class: '' },
-    //{ path: '/typography', title: 'Typography',  icon:'pe-7s-news-paper', class: '' },
-    //{ path: '/icons', title: 'Icons',  icon:'pe-7s-science', class: '' },
-    { path: '/maps', title: 'Maps',  icon:'pe-7s-map-marker', class: '' },
-    //{ path: '/notifications', title: 'Notifications',  icon:'pe-7s-bell', class: '' },
-    //{ path: '/upgrade', title: 'Upgrade to PRO',  icon:'pe-7s-rocket', class: 'active-pro' },
+  { path: '/dashboard', title: 'Dashboard', icon: 'pe-7s-graph', class: '' },
+  //{ path: '/user', title: 'User Profile',  icon:'pe-7s-user', class: '' },
+  //{ path: '/table', title: 'Table List',  icon:'pe-7s-note2', class: '' },
+  //{ path: '/typography', title: 'Typography',  icon:'pe-7s-news-paper', class: '' },
+  //{ path: '/icons', title: 'Icons',  icon:'pe-7s-science', class: '' },
+  { path: '/maps', title: 'Maps', icon: 'pe-7s-map-marker', class: '' },
+  //{ path: '/notifications', title: 'Notifications',  icon:'pe-7s-bell', class: '' },
+  //{ path: '/upgrade', title: 'Upgrade to PRO',  icon:'pe-7s-rocket', class: 'active-pro' },
 ];
 
 @Component({
@@ -25,15 +26,19 @@ export const ROUTES: RouteInfo[] = [
 export class SidebarComponent implements OnInit {
   menuItems: any[];
 
-  constructor() { }
+  constructor(private accountService: AccountService) { }
 
   ngOnInit() {
     this.menuItems = ROUTES.filter(menuItem => menuItem);
   }
   isMobileMenu() {
-      if ($(window).width() > 991) {
-          return false;
-      }
-      return true;
+    if ($(window).width() > 991) {
+      return false;
+    }
+    return true;
   };
+
+  logoutUser() {
+    this.accountService.logout();
+  }
 }
