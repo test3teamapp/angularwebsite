384d91c29005b350a8611be8660b555adca926e1
test3teamapp <test3teamapp@gmail.com>
2023-05-18 22:47:04 +0300

    update to use with traefik reverse proxy

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,7 +1,7 @@
 export const environment = {
   production: true,
-  redisapiEndpoint: 'http://rheotome.eu:8084',
-  socketiochatEndpoint:'http://rheotome.eu:3000',
+  redisapiEndpoint: 'https://gospy.rheotome.eu:8084',
+  socketiochatEndpoint:'https://gospy.rheotome.eu:3000',
   firebaseServerEndpointUrl: 'https://158.101.171.124:8082/api/v1/',
   endpointUrl: "https://158.101.171.124:8081/api/v1/",
   reconnectInterval: 2000
